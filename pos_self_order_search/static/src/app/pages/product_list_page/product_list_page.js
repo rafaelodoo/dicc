407c84a5353a1e2e437b67eba408f4d0dbcd8a58
Rafail <154279905+rafaelodoo@users.noreply.github.com>
2025-04-04 11:54:51 -0600

    ds

--- pos_self_order_search/static/src/app/pages/product_list_page/product_list_page.js
+++ pos_self_order_search/static/src/app/pages/product_list_page/product_list_page.js
@@ -1,23 +1,29 @@
 /** @odoo-module **/
 import { ProductListPage } from "@pos_self_order/app/pages/product_list_page/product_list_page";
 import { patch } from "@web/core/utils/patch";
+import { useState } from "@web/core/utils/hooks";
 
 patch(ProductListPage.prototype, {
     setup() {
         super.setup();
-        this.state.search = false;
-        this.state.searchInput = "";
+        this.searchState = useState({
+            isVisible: false,
+            input: "",
+        });
     },
 
     focusSearch() {
-        this.state.search = !this.state.search;
-        if (!this.state.search) {
-            this.state.searchInput = "";
+        this.searchState.isVisible = !this.searchState.isVisible;
+        if (!this.searchState.isVisible) {
+            this.searchState.input = "";
         }
     },
 
     getFilteredProducts(products) {
-        const searchInput = this.state.searchInput.toLowerCase();
+        const searchInput = this.searchState.input.toLowerCase();
+        if (!searchInput) {
+            return products;
+        }
         return products.filter(product => 
             product.name.toLowerCase().includes(searchInput) ||
             (product.description_self_order && product.description_self_order.toLowerCase().includes(searchInput))
