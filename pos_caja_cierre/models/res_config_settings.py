dd9aee6d045e7954c7606d4174b194dc287ff851
Rafail <rafalopez@dicc.com.mx>
2025-06-11 14:49:46 -0600

    df

--- pos_caja_cierre/models/res_config_settings.py
+++ pos_caja_cierre/models/res_config_settings.py
@@ -4,8 +4,6 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     hide_cash_details_on_closing = fields.Boolean(
-        related='pos_config_id.hide_cash_details_on_closing',
-        readonly=False,
         string='Ocultar detalles de efectivo al cerrar',
         help='Si está marcado, se ocultarán los detalles de efectivo al cerrar la caja',
         config_parameter='pos_caja_cierre.hide_cash_details_on_closing'
