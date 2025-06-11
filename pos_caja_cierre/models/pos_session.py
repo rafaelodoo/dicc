3bf544f6221dd0b97f6c56d36a5b53b9a81d4dfa
Rafail <rafalopez@dicc.com.mx>
2025-06-11 13:30:52 -0600

    fd

--- pos_caja_cierre/models/pos_session.py
+++ pos_caja_cierre/models/pos_session.py
@@ -4,3 +4,12 @@ class PosSession(models.Model):
     _inherit = 'pos.session'
 
     # Aquí irán los campos adicionales que necesitemos 
+
+class PosConfig(models.Model):
+    _inherit = 'pos.config'
+
+    hide_cash_details_on_closing = fields.Boolean(
+        'Ocultar Detalles de Efectivo en Cierre',
+        help='Si está marcado, los detalles de apertura, pagos y diferencia de efectivo se ocultarán en el popup de cierre de caja.',
+        default=False
+    ) 
