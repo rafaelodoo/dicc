411177dadb61744b4b348861d15fff0b44f59987
Rafail <rafalopez@dicc.com.mx>
2025-06-25 12:27:28 -0600

    rael

--- pos_customer_note/__manifest__.py
+++ pos_customer_note/__manifest__.py
@@ -1,30 +1,24 @@
 {
     'name': 'TPV Nota del Cliente',
-    'version': '18.0.1.0.0',
-    'category': 'Point of Sale',
-    'summary': 'Añade un botón para ver las notas del cliente en el TPV.',
+    'version': '1.0',
+    'category': 'Sales/Point of Sale',
+    'summary': 'Permite agregar una nota del cliente en el ticket del TPV',
     'description': """
-        Este módulo es un ejercicio práctico que añade un botón en la interfaz
-        del Punto de Venta para mostrar las notas internas del cliente seleccionado.
+Este módulo permite que el cajero agregue una nota personalizada del cliente en el ticket del TPV.
     """,
-    'author': 'rafael López Flores',
-    'website': 'https://www.dicc.com.mx',
-    'depends': [
-        'point_of_sale', # Nuestra dependencia fundamental
-    ],
+    'author': 'Tu Nombre o Empresa',
+    'website': 'https://tusitio.com',
+    'depends': ['point_of_sale'],
     'data': [
-        'views/pos_views.xml',
+        # Aquí puedes agregar otras vistas o archivos de datos si los tienes
     ],
     'assets': {
-        'point_of_sale._assets_pos': [
-            'pos_customer_note/static/src/app/components/customer_note_button/customer_note_button.xml',
+        'point_of_sale.assets_prod': [
             'pos_customer_note/static/src/app/components/customer_note_button/customer_note_button.js',
-            'pos_customer_note/static/src/app/components/customer_note_button/customer_note_button.scss',
             'pos_customer_note/static/src/app/screens/product_screen/product_screen.js',
         ],
     },
     'installable': True,
-    'application': False,
     'auto_install': False,
     'license': 'LGPL-3',
 }
