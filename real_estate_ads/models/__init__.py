5d836dd3a5487ce6aaed5637249da762af2a7599
Rafail <rafalopez@dicc.com.mx>
2025-06-30 11:55:45 -0600

    Primer commit

---
+++ real_estate_ads/models/__init__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Real Estate Ads',
+    'version': '1.0',
+    'category': 'Personalizado',
+    'summary': 'Este módulo es de prueba y solo es para fines educativos',
+    'description': """
+Esta es la descripción del modulo. El modulo solo esta creado con fines educativos.
+    """,
+    'author': 'Rafael López Flores',
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
