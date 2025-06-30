6cdb13cbac3ed02acbc4cddbd688f21131256596
Rafail <rafalopez@dicc.com.mx>
2025-06-30 17:29:33 -0600

    segundo commit

--- elearning_portal/__manifest__.py
+++ elearning_portal/__manifest__.py
@@ -1,11 +1,25 @@
 {
-    "name": "Mi componente de portal eLearning",
+    "name": "Mi componente de eLearning",
     "author": "CampusCleverit",
     "website": "https://campuscleverit.es",
     "version": "18.0.0.1.0",
     "summary": "Displays subscribed eLearning courses on the portal using OWL",
     "category": "Portal/eLearning",
-    "installable": true,
+    "depends": [
+        "portal",
+        "website_slides",
+        "web"
+    ],
+    "data": [
+        "views/portal_template.xml"
+    ],
+    "assets": {
+        "web.assets_frontend": [
+            "elearning_portal/static/src/components/*.js",
+            "elearning_portal/static/src/components/*.xml"
+        ]
+    },
+    
     "application": false,
     "auto_install": false,
     "license": "LGPL-3"
